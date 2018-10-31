ae431ba6e47f118628dd5814d32234c6e9b6317b
Richard Wardin <Shalmezad@gmail.com>
2018-10-30 20:30:27 -0400

    Added sliver crater only

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/auton/SliverCraterOnly.java
@@ -0,0 +1,10 @@
+package org.firstinspires.ftc.teamcode.opmodes.auton;
+
+public class SliverCraterOnly extends BaseAutoOp {
+    @Override
+    protected void runAuton() {
+        lowerRobot();
+        driveTrainDriveXInches(19.5);
+        
+    }
+}
